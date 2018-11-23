252906298ba84a3691651962177b863d6dbd396a
crispizza <hakgeumkim@gmail.com>
2018-11-23 13:01:11 +0900

    dd

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>hack'em</title>
+        <? php get_header(); ?>
+
+    </head>
+
+    <body>
+        <?php include "nav.php"; ?>
+
+
+        <!-- Top Center -- Banner VCR OSD Mono-->
+        <div class="content-container">
+            
+            <p>
+                    <iframe class="Video" width="560" height="315" src="https://www.youtube-nocookie.com/embed/vAKtNV8KcWg?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </p>
+
+            <!--
+            <p align=center>
+                    <img style='height: 100%; width: 100%; object-fit: contain' src="images/banner.gif">
+            </p>
+            -->
+            
+            <p>
+                <h1 style="font-size: 3vw;">HACK'EM KIM (HAK GEUM KIM), <br>
+                Game-creator; <span style="background-color: darksalmon">Email</span> hakgeumkim@gmail.com</h1>
+            </p>
+        
+            
+        </div>
+        <!-- Content -->
+        
+        <!--
+        <div class="row">
+            <div class="column">
+                <img src="images/AIW11.PNG" style="width:100%">
+                <img src="images/AIW8.PNG" style="width:100%">
+                <img src="images/AIW2.PNG" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="images/AIW9.PNG" style="width:100%">
+                <img src="images/AIW10.PNG" style="width:100%">
+                <img src="images/AIW7.PNG" style="width:100%">
+            </div>  
+        </div>
+        -->
+        
+    </body>
+</html>
